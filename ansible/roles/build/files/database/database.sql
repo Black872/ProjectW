1f1be20097ff6bad050b2f4d857e2843dee5050e
Dark Work <dark@gmail.com>
2023-11-14 23:41:42 +0200

    Added backend(not worked)

--- ansible/roles/build/files/database/database.sql
+++ ansible/roles/build/files/database/database.sql
@@ -1,1 +1,7 @@
-CREATEDB mydb
+CREATE TABLE weather (
+    city varchar(80),
+    temp_lo int, -- low temperature
+    temp_hi int, -- high temperature
+    prcp real, -- precipitation
+    date date
+);
